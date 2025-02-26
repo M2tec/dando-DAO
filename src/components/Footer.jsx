34d438b4093bb1db820209f86500354bfd08f1f4
M2tec <info@m2tec.nl>
2025-02-26 10:03:05 +0100

    Text color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,29 +9,29 @@ const Footer = () => {
             <div class="container">
                 <div class="row">
 
-                    <div class="col d-flex justify-content-end align-items-start">
+                    <div class="col d-flex justify-content-end align-items-start px-5">
                         <img style={{width:250}} src="/dandelion-logo.svg"></img>
                     </div>
 
                     <div class="col p-0">
                         <div class="container p-0">
-                            <div class="row">
-                                <a href="#" className="underline"><b>Dandelion-lite goverance</b></a>
+                            <div class="row ">
+                                <a class="text-white" href=""><b>Dandelion-lite goverance</b></a>
                             </div>                            
                             <div class="row">
-                                <a href="#" className="underline">Catalyst milestones</a>
+                                <a class="text-white" href="https://milestones.projectcatalyst.io/projects/1300078">Catalyst milestones</a>
                             </div>
                             <div class="row">
-                                <a href="#" className="underline">Gimbalabs Motion</a>
+                                <a class="text-white" href="https://www.notion.so/gimbalabs/Getting-Started-930b4f8d049b44ff964a0f7bd174d413">Gimbalabs Notion</a>
                             </div>
                             <div class="row">
-                                <a href="#" className="underline">Github</a>
+                                <a class="text-white" href="https://github.com/GameChangerFinance/dandelion-lite">Github</a>
                             </div>
                             <div class="row">
-                                <a href="#" className="underline">Gimbalabs Discord</a>
+                                <a class="text-white" href="https://discord.gg/nxf55MMhSp">Gimbalabs Discord</a>
                             </div>
                             <div class="row">
-                                <a href="#" className="underline">Gamechanger wallet Discord</a>
+                                <a class="text-white" href="https://discord.gg/PqXD4dbTfH">Gamechanger wallet Discord</a>
                             </div>
                         </div>
                     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,16 @@
 const NavBar = () => {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div className="container-fluid">
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item"><a className="nav-link" href="#">Home</a></li>
-              <li className="nav-item"><a className="nav-link" href="#">M2 Dandelion nodes</a></li>
-              <li className="nav-item"><a className="nav-link" href="#">M3 Education</a></li>
-              <li className="nav-item"><a className="nav-link" href="#">M4 Maintanance</a></li>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+              <li class="px-1 nav-item"><a class="nav-link text-white" href="#">Home</a></li>
+              <li class="px-1 nav-item"><a class="nav-link text-white" href="#">M2 Dandelion nodes</a></li>
+              <li class="px-1 nav-item"><a class="nav-link text-white" href="#">M3 Education</a></li>
+              <li class="px-1 nav-item"><a class="nav-link text-white" href="#">M4 Maintanance</a></li>
             </ul>
           </div>
         </div>
