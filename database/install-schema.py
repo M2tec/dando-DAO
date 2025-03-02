a6654b67992295708b98dc020bc22cb6e7a9fb3d
M2tec <info@m2tec.nl>
2025-03-02 19:04:22 +0100

    Fix font issue

--- database/install-schema.py
+++ database/install-schema.py
@@ -31,7 +31,7 @@ print(mutation)
 
 for i in range(0, 15):
   try:
-    r = requests.post('http://localhost:8080/admin', json={"query": mutation})
+    r = requests.post('http://alpha:8080/admin', json={"query": mutation})
   except requests.exceptions.ConnectionError:
     time.sleep(1)
     print("Waiting for connection")
@@ -40,7 +40,7 @@ message = r.json()
 
 for i in range(0, 15):
   if "errors" in message:
-    r = requests.post('http://localhost:8080/admin', json={"query": mutation})
+    r = requests.post('http://alpha:8080/admin', json={"query": mutation})
     message = r.json()  
     print("Waiting for db to be ready ... " + str(i))# , end="  ")
     time.sleep(3)
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { loadModule } from './services/cardanoWasm';
 import './index.css'
+import "@fontsource/poppins/700.css";
 
 const AppLoader=()=>{
   const [cslReady,setCSLReady]=useState(null);
