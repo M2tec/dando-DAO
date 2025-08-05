980c0bbd4250feca707d89130096e501193d8342
M2tec <info@m2tec.nl>
2025-08-05 09:41:11 +0200

    Fix env vars for monitor

--- push-docker-images.sh
+++ push-docker-images.sh
@@ -2,6 +2,8 @@
 
 docker compose --profile production up --build -d 
 
+docker login docker.io
+
 docker image tag dando-dao-server m2tec/dando-dao-server
 docker image tag dando-dao-monitor m2tec/dando-dao-monitor
 
